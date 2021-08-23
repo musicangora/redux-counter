3b61f48860af59fb04bb46ffb5fe40db40208f2b
Miiya <40447362+musicangora@users.noreply.github.com>
2021-08-24 03:40:29 +0900

    任意の数値でカウントアップする機能を実装

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -3,6 +3,7 @@ import styles from "./Counter.module.css";
 
 export function Counter() {
    const [count, setCount] = useState(0);
+   const [incrementAmount, setIncrementAmount] = useState("2");
   
    const increment = () => {
      setCount(count+1);
@@ -12,12 +13,29 @@ export function Counter() {
      setCount(count-1);
    }
 
+   const handleChange = (e) => {
+     setIncrementAmount(e.target.value);
+   }
+
+   const incrementByAmount = () => {
+     // Inputにはテキストが入るかもしれない
+     // Numberでキャストして、0と和集合を取ってテキスト入力時は0になるようにする
+     setCount(count+(Number(incrementAmount) || 0));
+   }
+
   return (
     <div>
       <div className={styles.row}>
         <button className={styles.button} onClick={increment}>+</button>
         <span className={styles.value}>{count}</span>
         <button className={styles.button} onClick={decrement}>-</button>
+      </div>
+
+      <div className={styles.row}>
+        <input className={styles.textbox} value={incrementAmount} onChange={handleChange} />
+        <button className={styles.button} onClick={incrementByAmount}>
+          Add Amount
+        </button>
       </div>
     </div>
   );
